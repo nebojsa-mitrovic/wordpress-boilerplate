802a2bf490db3153ef9d44d89246b96b2de586eb
nebojsa-mitrovic <32269043+Nebojsa-nm-d@users.noreply.github.com>
2021-02-23 07:38:42 +0100

    Refactoring

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <div>
-  <div>
-    <div>
-      <?php
-      while (have_posts()) : the_post();
-      ?>
-        <h2><a href="<?php the_permalink(); ?>" name="<?php the_ID(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-        <?php the_content(); ?>
-      <?php
-      endwhile;
-      ?>
-    </div>
-  </div>
+  <?php
+  while (have_posts()) : the_post();
+  ?>
+    <h2><a href="<?php the_permalink(); ?>" name="<?php the_ID(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+  <?php
+  endwhile;
+  ?>
 </div>
--- search.php
+++ search.php
@@ -10,60 +10,56 @@ get_header(); ?>
 
 <div class="search-post">
   <div>
-    <div>
-
-      <div>
-        <?php if (have_posts()) : ?>
-          <h1 class="page-title"><?php printf(__('Suchen: %s', 'wordpress-boilerplate'), '<span>' . get_search_query() . '</span>'); ?></h1>
-
-          <?php while (have_posts()) : the_post();
-            $post_image = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
-            $title = get_the_title();
-            $content = get_the_content();
-            $author = get_the_author();
-            $date = get_the_time("j F Y");
-            $tags = get_the_tags();
-          ?>
-            <div class="search-post__single">
-              <div>
-                <h2><a href="<?php echo get_post_permalink(); ?>"><?php echo $title ?></a></h2>
-              </div>
-              <div>
-                <?php _e('Author', 'wordpress-boilerplate'); ?>: <?php echo $author ?>
-              </div>
-              <div>
-                <?php _e('Daten', 'wordpress-boilerplate'); ?>: <?php echo $date ?>
-              </div>
-              <div>
-                <?php _e('Kategorien', 'wordpress-boilerplate'); ?>:
-                <?php the_category(', '); ?>
-              </div>
-              <div>
-                <?php _e('Tags', 'wordpress-boilerplate'); ?>:
-                <?php foreach (get_the_tags($post->ID) as $tag) {
-                  echo '<a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . ' </a>';
-                } ?>
-              </div>
-              <div>
-                <p><?php the_excerpt(); ?></p>
-              </div>
-            </div>
-          <?php endwhile; ?>
-
-          <div class="wordpress-boilerplate-pagination">
-            <?php echo paginate_links(array('prev_text' => __('&laquo;'), 'next_text' => __('&raquo;'))); ?>
+    <?php if (have_posts()) : ?>
+      <h1 class="page-title"><?php printf(__('Suchen: %s', 'wordpress-boilerplate'), '<span>' . get_search_query() . '</span>'); ?></h1>
+      <?php while (have_posts()) : the_post();
+        $post_image = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+        $title = get_the_title();
+        $content = get_the_content();
+        $author = get_the_author();
+        $date = get_the_time("j F Y");
+        $tags = get_the_tags();
+      ?>
+        <div class="search-post__single">
+          <div>
+            <h2><a href="<?php echo get_post_permalink(); ?>"><?php echo $title ?></a></h2>
           </div>
+          <div>
+            <?php _e('Author', 'wordpress-boilerplate'); ?>: <?php echo $author ?>
+          </div>
+          <div>
+            <?php _e('Daten', 'wordpress-boilerplate'); ?>: <?php echo $date ?>
+          </div>
+          <div>
+            <?php _e('Kategorien', 'wordpress-boilerplate'); ?>:
+            <?php the_category(', '); ?>
+          </div>
+          <div>
+            <?php _e('Tags', 'wordpress-boilerplate'); ?>:
+            <?php foreach (get_the_tags($post->ID) as $tag) {
+              echo '<a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . ' </a>';
+            } ?>
+          </div>
+          <div>
+            <p><?php the_excerpt(); ?></p>
+          </div>
+        </div>
+      <?php endwhile; ?>
 
-        <?php else : ?>
-          <h2><?php _e('Leider kein Ergebnis gefunden!', 'wordpress-boilerplate'); ?></h2>
-        <?php endif; ?>
+      <div class="wordpress-boilerplate-pagination">
+        <?php echo paginate_links(array('prev_text' => __('&laquo;'), 'next_text' => __('&raquo;'))); ?>
       </div>
-    </div>
 
-    <div class="sidebar-right">
+    <?php else : ?>
+      <h2><?php _e('Leider kein Ergebnis gefunden!', 'wordpress-boilerplate'); ?></h2>
+    <?php endif; ?>
+  </div>
+
+  <aside class="sidebar-right">
+    <div class="section">
       <?php dynamic_sidebar('archive_sidebar'); ?>
     </div>
-  </div>
+  </aside>
 </div>
 
 <?php get_footer(); ?>
